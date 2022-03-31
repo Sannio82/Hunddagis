4cc72a71bbe7e3f150c504efc88da129f748fe55
Sanna Johansson <susanna.johansson@iths.se>
2022-03-31 14:20:51 +0200

    API in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import DogDayCare from './records.json';
 import { useState } from 'react';
 import Welcome from './Components/Welcome';
 import AllDogs from './Components/AllDogs';
 import DogInfo from './Components/DogInfo';
+import React, { useEffect } from 'react';
 
-function App() {
+  const App = () => {
 
+      const [dogs, setDogs] = useState([]);
+
+    useEffect(() => {
+      const url = "https://api.jsonbin.io/b/6087d9c3f6655022c46d0b41";
+  
+      fetch(url)
+        .then (response => response.json())
+        .then (setDogs);     
+    }, []);
+      
   const WELCOME = 'welcome', ALLDOGS = 'alldogs', DOGINFO = 'doginfo';
   const [currentScreen, setCurrentScreen] = useState(WELCOME);
 
@@ -18,7 +30,8 @@ function App() {
       content = <Welcome nextScreen={() => setCurrentScreen(ALLDOGS)} /> 
       break;
     case ALLDOGS:
-      content = <AllDogs showDogInfo={() => setCurrentScreen(DOGINFO)} />
+      content = <AllDogs showAllDogs={dogs.map(dog =>
+        <div key={dog.chipNumber}>{dog.name}</div>)}  showDogInfo={() => setCurrentScreen(DOGINFO)} />
       break;
     case DOGINFO:
       content = <DogInfo startWelcome={() => setCurrentScreen(WELCOME)} />
@@ -29,11 +42,35 @@ function App() {
       <header className="App-header">
        <h1>Hunddagis</h1> 
       </header>
-      <main>
+      <main> 
+        <div>
+          {dogs.map(dog =>
+          <div key={dog.chipNumber}>{dog.name}</div>)}
+        </div>
         {content}
       </main>
     </div>
   );
 }
 
+
+
 export default App;
+
+
+// useEffect(() => {
+//   const url = "https://api.jsonbin.io/b/6087d9c3f6655022c46d0b41";
+
+//   const fetchData = async() => {
+//     try {
+//       const response = await fetch(url);
+//       const json = await response.json();
+//       console.log(json);
+//     } catch (error) {
+//       console.log("error", error);
+//     }
+//   };
+
+//   fetchData();
+  
+// }, []);
--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -2,8 +2,9 @@ const Welcome = ({nextScreen}) => {
     return (
         <section>
             <h2>Välkommen till mitt lilla hunddagis!</h2>
-            <p>Vill du fortsätta till nästa sida för att se alla våra små hundar?</p>
             <button onClick={nextScreen}>Ja tack!</button>
+            <p>Vill du fortsätta till nästa sida för att se alla våra små hundar?</p>
+            
         </section>
     )
 }
