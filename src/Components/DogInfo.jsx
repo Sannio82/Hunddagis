e9e8517dcf05cb6547a54835458f151ae186e289
Sanna Johansson <susanna.johansson@iths.se>
2022-04-16 09:37:03 +0200

    .

--- src/Components/DogInfo.jsx
+++ src/Components/DogInfo.jsx
@@ -19,17 +19,19 @@ const DogInfo = ({dogs}) => {
 
     return (
             <div className='alldogsbackground'>
-                        <img src={selectedDog.img} alt="Selected dog"/>              
+                        <img src={selectedDog.img} alt="Selected dog"/> 
+                        <center>               
         <div className='selectedDogCard' key={selectedDog.chipNumber} >  
-        <center>  
+       
         <div className='container'>        
                  <h2>{selectedDog.name}</h2> 
                <p>Jag heter {selectedDog.name} och jag är {selectedDog.age} år gammal! </p>
               <p>Jag är&nbsp;{selectedDog.present ? "inne på dagis." : "inte här på dagis just nu." }</p>
                <p>Min ägare är {selectedDog.owner.name}&nbsp;{selectedDog.owner.lastName}&nbsp;och har telefonnummer: {selectedDog.owner.phoneNumber}</p>
                </div>  
-               </center>    
+                 
             </div>
+            </center> 
             <Link to="/welcome">
             <button className='button'>Gå tillbaka till startsidan</button>
             </Link> 
